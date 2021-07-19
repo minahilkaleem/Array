8a9dcbf1f80a1111dfa763e228dd3223e739ddd9
Minahil Kaleem <minahilkaleem01@gmail.com>
2021-07-19 15:46:12 +0500

    Added

---
+++ Array.js
@@ -0,0 +1,19 @@
+// Setup Array
+var myArray = [["John", 10], ["Sami", 45], ["Prey", 3]];
+console.log(myArray);
+
+myArray.push(["Ray", 4]);
+console.log(myArray);
+
+var removeFromArray = myArray.pop(); // Remove last element form array
+console.log(myArray);
+
+var Arr = ["Stimpson", "John", "Xry"];
+console.log(Arr);
+
+var removeFromArr = Arr.shift(); // Remove first element from array
+console.log(removeFromArr); 
+Arr.unshift("Happy"); // Add element in first of array
+
+console.log(Arr);
+
